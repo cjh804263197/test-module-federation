860d43831c9fea64abe463d01d0755927fa8fec7
Eric Chan <mr_chenjiahui@163.com>
2021-07-01 10:51:47 +0800

    基于module-federation-examples/basic-host-remote的提交

---
+++ app1/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.js$/,
+        loader: "bundle-loader",
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  //http://localhost:3002/remoteEntry.js
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      remotes: {
+        app2: `app2@${getRemoteEntryUrl(3002)}`,
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+function getRemoteEntryUrl(port) {
+    return `//localhost:${port}/remoteEntry.js`
+}
